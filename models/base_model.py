92fcd70eaeb3b22d704d677d8e104a07cf0f9de5
JasonFlair <emelieobumse100@gmail.com>
2022-12-03 17:00:54 +0100

    sql

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,18 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData, Column, Integer, String, DateTime
+
+basemetadata = MetaData()
+Base = declarative_base(metadata=basemetadata)
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -54,4 +62,13 @@ class BaseModel:
                                (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        for k, v in dictionary.keys():
+            if k == '_sa_instance_state':
+                del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """
+        deletes self instance from storage
+        """
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import MetaData, Column, Integer, String, DateTime, ForeignKey
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+
+    @property
+    cities
