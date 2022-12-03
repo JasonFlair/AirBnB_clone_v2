df752b776f25baed63f1f4648664c3ca3c548a7f
JasonFlair <emelieobumse100@gmail.com>
2022-12-03 13:23:56 +0100

    create

--- console.py
+++ console.py
@@ -114,24 +114,25 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        arg_list = args.split()
-        kw = {}
-
-        if not args:
+        try:
+            if not args:
+                raise SyntaxError()
+            arg_list = args.split(" ")
+            kw = {}
+            for arg in arg_list[1:]:
+                keyword_split = arg.split("=")
+                keyword_split[1] = eval(keyword_split[1])
+                if type(keyword_split[1]) is str:
+                    keyword_split[1].replace("_", " ").replace('"', '\\"')
+                kw[keyword_split[0]] = keyword_split[1]
+        except SyntaxError:
             print("** class name missing **")
-            return
-        elif arg_list[0] not in HBNBCommand.classes:
+        except NameError:
             print("** class doesn't exist **")
-            return
-        for arg in arg_list[1:]:
-            keyword_split = arg.split("=")
-            keyword_split[1] = eval(keyword_split[1])
-            if type(keyword_split[1]) is str:
-                keyword_split[1].replace("_", " ").replace('"', '\\"')
-            kw[keyword_split[0]] = keyword_split[1]
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+        new_instance.save()
         print(new_instance.id)
+
 
     def help_create(self):
         """ Help information for the create method """
