023a54551b8d45ad78e8f6dc0e701a504c4bfa1a
JasonFlair <emelieobumse100@gmail.com>
2022-12-03 14:38:04 +0100

    create user

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- creates a database, new user and grants privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db` TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema` TO 'hbnb_dev'@'localhost';
