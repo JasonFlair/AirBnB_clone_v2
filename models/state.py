a01cca4ad396674ae024b4a0534309af1382ebb8
JasonFlair <emelieobumse100@gmail.com>
2022-12-04 13:04:03 +0100

    fixed tablename

--- models/state.py
+++ models/state.py
@@ -9,7 +9,7 @@ from models.city import City
 
 class State(BaseModel):
     """ State class """
-    __tablename__ == "states"
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship(
         "City",
