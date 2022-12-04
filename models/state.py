de22d3ab66415bab9d1d3d9144d7e8f0798eea9c
JasonFlair <emelieobumse100@gmail.com>
2022-12-04 13:10:26 +0100

    fixed tablename

--- models/state.py
+++ models/state.py
@@ -7,7 +7,7 @@ import models
 from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
