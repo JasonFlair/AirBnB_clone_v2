825a273dc4654c48856844271ef01e0eb0f31f26
JasonFlair <emelieobumse100@gmail.com>
2022-12-04 13:01:23 +0100

    task 6

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel
 from sqlalchemy import MetaData, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
 class State(BaseModel):
@@ -12,7 +14,19 @@ class State(BaseModel):
     cities = relationship(
         "City",
         back_populates="state",
-        cascade="all, delete",
+        cascade="all, delete, delete-orphan",
         passive_deletes=True,
     )
 
+    @property
+    def cities(self):
+        """
+         A getter attribute 'cities' that returns the list of City
+         instances with state_id equals to the current State.id
+        """
+        list_of_cities = []
+
+        for city in models.storage.all(City):
+            if city.state_id == self.id:
+                list_of_cities.append(city)
+        return list_of_cities
