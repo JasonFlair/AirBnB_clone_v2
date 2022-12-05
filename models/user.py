f254bec8e359825b53856a50939ba7d2d59b68aa
JasonFlair <emelieobumse100@gmail.com>
2022-12-05 17:22:44 +0100

    amenities

--- models/user.py
+++ models/user.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import MetaData, Column, Integer, String
 from sqlalchemy.orm import relationship, backref
+from models.place import Place
+from models.review import Review
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class User(BaseModel, Base):
