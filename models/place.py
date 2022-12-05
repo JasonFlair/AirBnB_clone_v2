2c05711bb3a153684996a2742fb89c5a750ff6c2
JasonFlair <emelieobumse100@gmail.com>
2022-12-05 16:05:55 +0100

    amenities

--- models/place.py
+++ models/place.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 import models
-from models import Review, Amenity
+from models.review import Review
+from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from sqlalchemy import MetaData, Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
