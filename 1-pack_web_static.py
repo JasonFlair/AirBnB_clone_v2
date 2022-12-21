37611bc6235d2a55b4204cfe79e17231eec143c0
JasonFlair <emelieobumse100@gmail.com>
2022-12-21 21:17:30 +0100

    hm

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,14 +14,13 @@ def do_pack():
     tarfile_name = "web_static_" + str(datetime.datetime.now().year) + str(datetime.datetime.now().month) + str(
         datetime.datetime.now().day) + str(datetime.datetime.now().minute) + str(
         datetime.datetime.now().second) + ".tgz"
-    command = "tar -C versions -czvf {} web_static/".format(tarfile_name)
+    local("mkdir -p versions")
+    command = "tar -C versions -czvf {} ~/AirBnB_clone_v2/web_static/".format(tarfile_name)
 
     # create the tar file
     with settings(warn_only=True):
         """
         warn_only setting is set to True, so the local() function will not raise an exception 
         if the command returns a non-zero exit code."""
-        local_command = local(command)
-        if local_command.failed:
-            local("mkdir versions")
-            local(command)
+        local(command)
+
