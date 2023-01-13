17de42abd85b4db8ec633b7ac1233efa575618e7
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2023-01-13 15:15:06 +0100

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A simple script that starts a Flask web application which listens on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text
+/python/(<text>): display “Python ”, followed by the value of the text variable
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C %s' % escape(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
