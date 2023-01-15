2d2d2e1ec211ca3935d5362d4c826994031a332c
JasonFlair <emelieobumse100@gmail.com>
2023-01-15 12:39:14 +0100

    check

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a  script that starts a Flask web application which listens on 0.0.0.0"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_of_cities():
+    """ returns a state list, sorted by name """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def calls_close(exc):
+    """closes the current SQLAlchemy session."""
+    storage.close()
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
