ddc814f6c8af34af009a62ebb4b5d3a1278a3afd
JasonFlair <emelieobumse100@gmail.com>
2023-01-15 09:06:10 +0100

    check

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,13 +2,14 @@
 """a  script that starts a Flask web application which listens on 0.0.0.0"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def list_of_states():
     """ returns a state list, sorted by name """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template('7-state_lists.html', states=states)
 
 @app.teardown_appcontext
