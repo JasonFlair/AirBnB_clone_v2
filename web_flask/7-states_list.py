cfafab7b27532bc4aebc1208779a1705054ca8a7
JasonFlair <emelieobumse100@gmail.com>
2023-01-15 08:01:03 +0100

    okay

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,15 +5,16 @@ from models import storage
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def calls_close():
-    storage.close()
-
 @app.route('/states_list', strict_slashes=False)
 def list_of_states():
     """ returns a state list """
     states = storage.all("State")
     return render_template('7-state_lists.html', states=states)
 
+@app.teardown_appcontext
+def calls_close(error):
+    """closes the current SQLAlchemy session."""
+    storage.close()
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
